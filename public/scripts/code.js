4818b6ecd4685269d2895721c229c057b9765497
Alicia <23alicia.piyi@glimnet.se>
2025-01-23 11:09:00 +0200

    Work on server.js, connection to websocket

--- public/scripts/code.js
+++ public/scripts/code.js
@@ -1,1 +1,14 @@
 console.log("Hello World");
+
+// USE WEBSOCKET
+const websocket = new WebSocket("ws://localhost:8082");
+
+
+setTimeout(() => {
+    // SEND MESSAGE TO SERVER
+    let msg = {type: "chat", message: "Hello World"};
+
+    websocket.send(JSON.stringify(msg));
+    console.log("Meddelande skickades till servern");
+
+}, 5000);
--- server.js
+++ server.js
@@ -14,6 +14,39 @@ app.use(express.static("public"));
 // HTTP
 const server = http.createServer(app);
 
+// CREATE WEBSOCKET SERVER
+const wss = new WebSocketServer({ noServer: true });
+
+// HANDLE WEBSOCKET COMMUNICATION
+server.on('upgrade', (req, socket, head) => {
+
+    console.log(`Client Upgrade ...`);
+
+    wss.handleUpgrade(req, socket, head, (ws) => {
+        console.log(`Client Connected...`);
+
+        
+        wss.emit('connection', ws, req);
+
+    });
+
+});
+
+// LISTEN TO WEBSOCKET
+wss.on('connection', (ws) => {
+    console.log("New client connection");
+
+    // LISTEN TO EVENT
+    ws.on('message', (stream) => {
+
+        const obj = JSON.parse(stream);
+
+        // MESSAGE RECIEVED
+        console.log(obj.message);
+    })
+});
+
+
 server.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`);
 });
